e7d2c66e7f1973cc144d098237a46de6635de30c
Sinan ÇIVAK <sinancivak00@gmail.com>
2024-01-29 23:19:40 +0300

    Model Added

---
+++ Entities/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Message
+    {
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+    }
+}
